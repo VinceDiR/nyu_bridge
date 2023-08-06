622923c11f72e1eadc4e52c2cef96231b9d25d80
Nate <88605064+NateDiR@users.noreply.github.com>
2023-08-05 20:50:01 -0400

    format

--- exam1/nvd220_challenge_question.cpp
+++ exam1/nvd220_challenge_question.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     int n;
     std::cout << "Enter the size of the Barn Door: ";
     std::cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == 0 || i == n-1 || j == 0 || j == n-1) { // Borders
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+            {
                 std::cout << "#";
-            } else if (i-1 == j-1 || n-i-2 == j-1) { // Diagonals inside the (n-2)x(n-2) square
+            }
+            else if (i - 1 == j - 1 || n - i - 2 == j - 1)
+            {
                 std::cout << "$";
-            } else { // Rest of the space
+            }
+            else
+            {
                 std::cout << " ";
             }
         }
--- exam1/nvd220_coding_question1.cpp
+++ exam1/nvd220_coding_question1.cpp
@@ -2,26 +2,31 @@
 #include <cmath>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter the size of the diamond: ";
     cin >> n;
     char letter = 'a';
-    for(int i = -n+1; i < n; i++) {
+    for (int i = -n + 1; i < n; i++)
+    {
         int abs_i = abs(i);
-        for(int j = 0; j < abs_i; j++) {
+        for (int j = 0; j < abs_i; j++)
+        {
             cout << " ";
         }
 
-        if(i == -n+1 || i == n-1) {
-            cout << char(letter+n-abs_i-1) << "\n";
+        if (i == -n + 1 || i == n - 1)
+        {
+            cout << char(letter + n - abs_i - 1) << "\n";
             continue;
         }
-        cout << char(letter+n-abs_i-1);
-        for(int j = 0; j < 2*(n-abs_i)-3; j++) {
+        cout << char(letter + n - abs_i - 1);
+        for (int j = 0; j < 2 * (n - abs_i) - 3; j++)
+        {
             cout << " ";
         }
-        cout << char(letter+n-abs_i-1) << "\n";
+        cout << char(letter + n - abs_i - 1) << "\n";
     }
     return 0;
 }
