604133707e20744eb4efe7aa47880a7457322b8a
Nate D <88605064+NateDiR@users.noreply.github.com>
2023-08-10 13:46:56 -0400

    timer

---
+++ timer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+    int minutes;
+
+    // Ask the user for the duration in minutes
+    cout << "Enter the duration in minutes: ";
+    cin >> minutes;
+    int totalSeconds = minutes * 60;
+
+    // Start the timer countdown
+    for (int i = totalSeconds; i >= 0; --i)
+    {
+        cout << "\rTime left: " << i / 60 << " minutes " << i % 60 << " seconds" << flush;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+
+    cout << "\nTime's up!\n";
+    return 0;
+}
