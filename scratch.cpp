0a09278664fb2551a562c029641c2f1ec2777ce4
Nate <88605064+NateDiR@users.noreply.github.com>
2023-08-10 18:47:19 -0400

    scratch change

--- scratch.cpp
+++ scratch.cpp
@@ -1,53 +1,42 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    int n;
-    cout << "Please enter a positive integer:";
-    cin >> n;
+    int total_inches = 0;
+    int inches, days = 0;
 
-    if (n <= 0)
+    // Constants
+    const int INCHES_IN_A_FOOT = 12;
+    const int FEET_IN_A_YARD = 3;
+    const int INCHES_IN_A_YARD = INCHES_IN_A_FOOT * FEET_IN_A_YARD;
+
+    while (true)
     {
-        cout << "You entered a non-positive number. Please enter a positive integer." << endl;
-        return 1; // exit with error code
+        cout << "Enter the number of inches the snail traveled in day #"
+             << days + 1
+             << ", or type -1 if they reached their destination:";
+        cin >> inches;
+
+        if (inches == -1)
+        {
+            break;
+        }
+
+        total_inches += inches;
+        days++;
     }
 
-    // First half of the shape
-    for (int i = 1; i <= 2 * n - 1; ++i)
-    {
-        // Printing spaces
-        for (int j = 1; j <= 2 * n - i; ++j)
-        {
-            cout << " ";
-        }
+    int yards = total_inches / INCHES_IN_A_YARD;
+    total_inches %= INCHES_IN_A_YARD;
 
-        // Printing asterisks
-        for (int j = 1; j <= i; ++j)
-        {
-            cout << "*";
-        }
+    int feet = total_inches / INCHES_IN_A_FOOT;
+    total_inches %= INCHES_IN_A_FOOT;
 
-        cout << endl;
-    }
-
-    // Second half of the shape
-    for (int i = 2 * n - 2; i >= 1; --i)
-    {
-        // Printing spaces
-        for (int j = 1; j <= 2 * n - i; ++j)
-        {
-            cout << " ";
-        }
-
-        // Printing asterisks
-        for (int j = 1; j <= i; ++j)
-        {
-            cout << "*";
-        }
-
-        cout << endl;
-    }
+    cout << "In " << days << " days, the snail traveled " << yards
+         << " yards, " << feet << " feet and " << total_inches << " inches."
+         << endl;
 
     return 0;
 }
