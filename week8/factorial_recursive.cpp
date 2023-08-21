3c7e09b421a8f48aa46eef05d8756890a7d7fb05
Nate <88605064+NateDiR@users.noreply.github.com>
2023-08-20 23:28:37 -0400

    factorial recursive

--- week8/factorial_recursive.cpp
+++ week8/factorial_recursive.cpp
@@ -21,5 +21,12 @@ int main()
 
 int factorial(int n)
 {
-    return (n == 0) ? 1 : n * factorial(n - 1);
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
 }
